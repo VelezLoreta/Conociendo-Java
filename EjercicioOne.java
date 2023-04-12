8775d285aa8e52dae571985d2114905f742453de
loretta <velezloreta@gmail.com>
2023-04-12 11:26:37 -0300

    ejercicios

---
+++ EjercicioOne.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.one;
+
+/**
+ *
+ * @author lore_
+ */
+public class EjercicioOne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     int num1 = 5;
+      int num2 = 5;
+      int sumaa = num1 +num2;
+      boolean logico = num2 > num1;
+      num1++;
+      System.out.println ("El resultado es " + sumaa  );
+        
+      
+        
+    }
+    
+}
---
+++ EjercicioTwo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.two;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lore_
+ */
+public class EjercicioTwo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+    String nombre;
+        System.out.println("Ingresa tu nombre:");
+        nombre = leer.nextLine();
+        System.out.println("Hola " + nombre + " estamos encantados de tenerte en JAVA!");
+        
+    
+}
+}
